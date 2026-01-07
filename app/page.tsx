ad599a75ac7884fb4e90e4aa8305243e8c2bb4cf
Samir Azimov <samirazimov488@gmail.com>
2026-01-08 02:00:12 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import Banner from "@/components/Banner";
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <Banner />
+    </div>
+    </>
+  );
+}
+// 37f3b8d6-c8a3-45eb-ba13-c6c31ead9a5a
